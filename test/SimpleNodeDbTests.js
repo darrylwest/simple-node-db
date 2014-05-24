44f180fbb29932548b60d096df269b6f865912cf
darryl west <darryl.west@RainCitySoftware.com>
2014-05-24 14:30:19 -0700

    file tests

--- test/SimpleNodeDbTests.js
+++ test/SimpleNodeDbTests.js
@@ -6,29 +6,45 @@
  */
 var should = require('chai').should(),
     dash = require('lodash'),
-    SimpleNodeDb = require('../lib/SimpleNodeDb');
+    SimpleNodeDb = require('../lib/SimpleNodeDb' ),
+    fs = require('fs');
 
 describe('SimpleNodeDb', function() {
     'use strict';
 
-    var log = require('simple-node-logger' ).createLogger(),
-        createOptions;
-
-    createOptions = function() {
-        var opts = {};
-        opts.log = log;
-
-        return opts;
-    };
+    var dbfile = './simpledb-test';
 
     describe('#instance', function() {
-
+        var methods = [
+            'query',
+            'find',
+            'update',
+            'insert',
+            'delete',
+            'replicate',
+            'isMemoryOnly',
+            'dbPath'
+        ];
 
         it('should create a memory-only instance of SimpleNodeDb', function() {
             var db = new SimpleNodeDb();
             should.exist( db );
 
             db.should.be.instanceof( SimpleNodeDb );
+
+            db.isMemoryOnly().should.equal( true );
+        });
+
+        it('should create a file-based instance of SimpleNodeDb', function(done) {
+            var db = new SimpleNodeDb( dbfile );
+
+            should.exist( db );
+
+            fs.exists( dbfile, function(exists) {
+                exists.should.equal( true );
+
+                done();
+            });
         });
     });
 });
